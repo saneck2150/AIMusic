14a1589942983988219fcf80ef90389ab921815b
Oleksandr Mykhailyshyn <saneck2150@gmail.com>
2024-04-16 15:40:39 +0200

    Dataset upd

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,12 +1,16 @@
-import ms3
 import csv
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+PATH = 'beethoven_piano_sonatas\chords\\01-1.chords.tsv'
 
 table:list[str] = []
-with open('beethoven_piano_sonatas\chords\\01-1.chords.tsv', newline='') as csvfile:
+with open(PATH, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter='\t')
     for row in spamreader:
         table.append(row)
 
-output_file = 'tab_str.tsv'
+        
 
